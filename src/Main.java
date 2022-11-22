eff1b573df0129b1fadf985386fc9285151d3397
doughertybd <doughebd@mail.uc.edu>
2022-11-22 14:03:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 22.75;
+        double summerCost = 84.92;
+        double fallCost = 62.73;
+        double winterCost = 93.15;
+        double totalCost;
+        totalCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The cost of the four seasons (spring: $" + springCost + ", summer: $" + summerCost + ", fall: $" + fallCost + ", winter: $" + winterCost + ") totaled up is: $" + totalCost);
+    }
+}
